feb980178898d0d32cbfaa71d78aef3e90d9bc68
ShouvikPSarker <shouviksarker94@gmail.com>
2025-01-17 18:22:37 +0530

    minor changes in app

--- src/app.js
+++ src/app.js
@@ -5,4 +5,11 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cookieParser())
+app.use(express.static("Public"))
+app.use(express.json({ limit: '20kb' }));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 export default app;
